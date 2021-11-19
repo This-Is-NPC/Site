63ff8daad98948b9827bb3a5a9c17ee5dee3937b
Gabriel Follone <gabrielfollone27@gmail.com>
2021-11-19 19:10:03 -0300

    Primeira página

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,9 @@ function Home() {
         <h2>
             O mundo da tecnologia
         </h2>
+        <h3>
+        <a href="https://shopee.com.br/shop/326383562/">Visite a nossa loja!</a>
+        </h3>
     </div>
 }
 export default Home
